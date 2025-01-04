63861fcd204c6f4a51db3eadbcb54169159cf81e
Ankur Gupta <ankur1992@gmail.com>
2025-01-04 11:41:48 +0530

    Working code

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/", response_class=HTMLResponse)
 async def get_form(request: Request):
-    return templates.TemplateResponse("form.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "message":None})
 
-@app.post("/submit", response_class=HTMLResponse)
+@app.post("/", response_class=HTMLResponse)
 async def handle_form(request: Request, user_input: str = Form(...)):
     response = f"Server received: {user_input}"
-    return templates.TemplateResponse("response.html", {"request": request, "response": response})
+    return templates.TemplateResponse("index.html", {"request": request, "response": response})
 
 if __name__ == "__main__":
     import uvicorn
